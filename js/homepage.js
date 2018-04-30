4c5cb4ed53d6f205a5656b3a38a19cd51b3a9337
Antonia Pang <i7699172@bournemouth.ac.uk>
2018-04-30 19:23:37 +0100

    everything but about

--- js/homepage.js
+++ js/homepage.js
@@ -20,4 +20,55 @@ $('#mobile-menu-list').toggleClass('d-none d-inline-block');
 
   });
 
+
+
+  $('.batshuayi-text').click(function(){
+    $('#batshuayi').fadeIn( "slow", function() {
+  });
+  });
+
+
+  $('.bat-close').click(function(){
+    $('#batshuayi').fadeOut( "slow", function() {
+  });
+  });
+
+    $('.brewster-text').click(function(){
+    $('#Brewster').fadeIn( "slow", function() {
+  });
+  });
+
+
+  $('.brew-close').click(function(){
+    $('#Brewster').fadeOut( "slow", function() {
+  });
+  });
+
+    $('.sterling-text').click(function(){
+    $('#sterling').fadeIn( "slow", function() {
+  });
+  });
+
+
+  $('.ster-close').click(function(){
+    $('#sterling').fadeOut( "slow", function() {
+  });
+  });
+
+    $('.anton-text').click(function(){
+    $('#anton').fadeIn( "slow", function() {
+  });
+  });
+
+
+  $('.anton-close').click(function(){
+    $('#anton').fadeOut( "slow", function() {
+  });
+  });
+
+
+
+
+
+
 });

af3fc177f10693129d00d3f333b3eb380fa7f90b
Antonia Pang <i7699172@bournemouth.ac.uk>
2018-05-01 10:18:38 +0100

    test

--- js/homepage.js
+++ js/homepage.js
@@ -2,17 +2,6 @@ $(document).ready(function(){
 
 
 
-  $('a[href^="#"]').on('click', function(event) {
- var target = $( $(this).attr('href') );
-if(target.length) {
- event.preventDefault();
- $('html, body').animate({
-   scrollTop: target.offset().top
-  }, 1000);
-  }
-  });
-
-
 
   $('.mobile-menu-icon').click(function(){
 
